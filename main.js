bab2d4769ea7a213e31b9240aac6609928292835
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2024-08-05 14:41:55 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+var n = 20;
+console.log(typeof(n));
+
+var s = "";
+console.log(typeof(s));
+
+var o = null;
+console.log(typeof(o));
+
+
+var b = true;
+console.log(typeof(b));
+
+var f = ()=>{};
+console.log(f);
+
+
+function Função(params) {}
+console.log(typeof(Função));
+
+// OPERADORES LOGICOS
+
+var n = 3;
+// n = n - 10;
+// var m = n * 10;
+// var d = n / 40;
+  n %= 200;
+console.log(n);
+
+var x =5;
+// x++
+// x--
+console.log(x);
+
+// COMPARAÇÃO
+a = 50;
+b = 100
+console.log(n < x);
+console.log(n > x);
+console.log(n == x);
+console.log(n <= x);
+console.log(n >= x);
+console.log(n != x);
+
+console.log(n != x && a < b);
+console.log(n != x || a >= b);
+console.log(n != x || a != b);
+console.log(!(n != x || a != b));
+
+
+
+
